dfa5c118a8852ab6a7dfec92cfce83ac12d871b5
Koen van Benthem <koenvanbenthem@gmail.com>
2017-02-16 18:42:34 +0100

    adding labels on the connection lines in plot_tree

--- R/plot_tree.R
+++ R/plot_tree.R
@@ -1,10 +1,14 @@
+arrows_label <- function(x1,y1,x2,y2,lbl,...){
+  text(0.5*x1+0.5*x2,0.5*y1+0.5*y2,lbl)
+  arrows(x1,y1,x2,y2,...)
+}
+
 draw_jcr_tree <- function(forest,n){
   left <- forest$ldaughter[,n] + 1
   right <- forest$rdaughter[,n] + 1
   vars <- forest$node_var[,n] + 1
   thrs <- forest$node_xvar[,n]
   tst <- paste(vars,round(thrs,2),sep = " ? ")
-  print(tst)
   df <- data.frame(test=tst,left=left,right=right,parent=NA,x=0,y=0)
   df2 <- df[1:(nrow(df)+1-min(which(rev(df$test) != "0 ? 0.0"))),]
   
@@ -24,26 +28,21 @@ draw_jcr_tree <- function(forest,n){
 
     if(df2$left[i] != 1){    
     offs <- 2^(Nlayer-df2$y[i]-2)
-    df2$x[df2$left[i]] <- df2$x[i] - offs
-    df2$x[df2$right[i]] <- df2$x[i] + offs
+    
+    # change + offs and - offs to make the left child also appear on the left side.
+    df2$x[df2$left[i]] <- df2$x[i] + offs
+    df2$x[df2$right[i]] <- df2$x[i] - offs
 
-    arrows(df2$x[i],-df2$y[i],df2$x[df2$left[i]],-df2$y[df2$left[i]],angle=0,col="grey")
-    arrows(df2$x[i],-df2$y[i],df2$x[df2$right[i]],-df2$y[df2$right[i]],angle=0,col="grey")
+    arrows_label(df2$x[i],-df2$y[i],df2$x[df2$left[i]],-df2$y[df2$left[i]],lbl=">",angle=0,col="grey")
+    
+    arrows_label(df2$x[i],-df2$y[i],df2$x[df2$right[i]],-df2$y[df2$right[i]],lbl="<",angle=0,col="grey")
+    
     text(df2$x[i],-df2$y[i],df2$test[i])
 
     } else if(df2$y[i] != 0) {
       text(df2$x[i],-df2$y[i],forest$yf_pred[i],col="green")
     }
   }
-  # Determining the coordinates
   
-  ##
-
-  #text(0.5,0.5,"oooo")
   return(df2)
 }
-
-plot(0,0,xlim=c(-16,16),ylim=c(-5,0),type="n")
-for(i in 0:5){
-  points((1:2^i)*32/(2^(i)+1)-16,-rep(i,length(1:2^i)))
-}    
